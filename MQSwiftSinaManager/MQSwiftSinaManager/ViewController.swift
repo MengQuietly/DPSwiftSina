2bd5660939070f7ee7621cca258f284d5d201cab
文静 <wenjing@txcap.com>
2016-05-19 16:02:08 +0800

    Project create and pods create

---
+++ MQSwiftSinaManager/MQSwiftSinaManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MQSwiftSinaManager
+//
+//  Created by 文静 on 16/5/18.
+//  Copyright © 2016年 mengQuietly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
