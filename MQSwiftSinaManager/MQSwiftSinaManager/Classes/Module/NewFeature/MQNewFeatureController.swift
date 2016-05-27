dab50f8d494e6dcd0f960c730f09c88713d240a8
文静 <wenjing@txdev.org>
2016-05-27 16:17:32 +0800

    新特性界面：通过代理方法动画显示启动按钮

--- MQSwiftSinaManager/MQSwiftSinaManager/Classes/Module/NewFeature/MQNewFeatureController.swift
+++ MQSwiftSinaManager/MQSwiftSinaManager/Classes/Module/NewFeature/MQNewFeatureController.swift
@@ -74,40 +74,25 @@ class MQNewFeatureController: UICollectionViewController {
     }
 
     // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(collectionView: UICollectionView, shouldHighlightItemAtIndexPath indexPath: NSIndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(collectionView: UICollectionView, shouldSelectItemAtIndexPath indexPath: NSIndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(collectionView: UICollectionView, shouldShowMenuForItemAtIndexPath indexPath: NSIndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(collectionView: UICollectionView, canPerformAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) -> Bool {
-        return false
-    }
-
-    override func collectionView(collectionView: UICollectionView, performAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) {
     
+    // 此代理方法中的 indexPath 参数是之前显示的 cell 的 indexPath，所以获取当前 cell 需自己获取
+    override func collectionView(collectionView: UICollectionView, didEndDisplayingCell cell: UICollectionViewCell, forItemAtIndexPath indexPath: NSIndexPath) {
+        
+        // 取出当前显示的 Cell 的indexPath
+        let path = collectionView.indexPathsForVisibleItems().last!
+        
+        // 是否为最后一个item
+        if path.item == MQNewFeatureImageCount - 1  {
+            // 获取cell
+            let cell = collectionView.cellForItemAtIndexPath(path)  as! MQNewFeatureControllerCell
+            cell.startBtnWithAnimationShow()
+        }
     }
-    */
-
 }
 
 /// 新特性 Cell，private 保证此 cell 只被当前控制器使用
 private class MQNewFeatureControllerCell: UICollectionViewCell{
+    // frame 的大小来自于 layout 的 itemSize
     override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -115,26 +100,58 @@ private class MQNewFeatureControllerCell: UICollectionViewCell{
     }
     
     required init?(coder aDecoder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
         super.init(coder: aDecoder)
         setUpUI()
     }
     
-    var iconIndex = 0 {
+    // 图像索引属性
+    private var iconIndex = 0 {
         didSet {
             iconView.image = UIImage(named: "new_feature_\(iconIndex + 1)")
+            startBtn.hidden = true
         }
     }
     
-    // MARK: - 懒加载属性：图像视图
+    // MARK: - 懒加载属性：
+    // 图像视图
     private lazy var iconView = UIImageView()
+    // 开始按钮
+    private lazy var startBtn : UIButton = {
+        let startBtns = UIButton()
+    
+        startBtns.setBackgroundImage(UIImage(named:"new_feature_button"), forState: UIControlState.Normal)
+        startBtns.setBackgroundImage(UIImage(named:"new_feature_button_highlighted"), forState: UIControlState.Highlighted)
+        startBtns.sizeToFit()
+        return startBtns
+    }()
+    
     
     // 设置界面元素
     private func setUpUI(){
         // 添加控件
         addSubview(iconView)
+        addSubview(startBtn)
         
         // 指定布局
         iconView.frame = bounds
+        
+        // 自动布局约束是添加在父视图上的
+        startBtn.translatesAutoresizingMaskIntoConstraints = false
+        addConstraint(NSLayoutConstraint(item: startBtn, attribute: NSLayoutAttribute.CenterX, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.CenterX, multiplier: 1.0, constant: 0))
+        addConstraint(NSLayoutConstraint(item: startBtn, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Bottom, multiplier: 1.0, constant: -80))
+    }
+    
+    // 动画显示启动按钮
+    private func startBtnWithAnimationShow(){
+        startBtn.hidden = false
+        startBtn.transform = CGAffineTransformMakeScale(0, 0)
+        // usingSpringWithDamping：弹性系数，0-1，越小越弹
+        // initialSpringVelocity：初始速度
+        UIView.animateWithDuration(1.2, delay: 0.0, usingSpringWithDamping: 0.8, initialSpringVelocity: 10, options: [], animations: { 
+            self.startBtn.transform = CGAffineTransformIdentity
+            }) { (_) in
+                
+        }
+        
     }
 }
