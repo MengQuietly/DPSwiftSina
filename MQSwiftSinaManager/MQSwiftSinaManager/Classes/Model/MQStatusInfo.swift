e465d1653be58777942dd293dfbd09c92435d28c
文静 <wenjing@txdev.org>
2016-05-30 11:38:25 +0800

    首页:user模型搭建

--- MQSwiftSinaManager/MQSwiftSinaManager/Classes/Model/MQStatusInfo.swift
+++ MQSwiftSinaManager/MQSwiftSinaManager/Classes/Model/MQStatusInfo.swift
@@ -18,6 +18,8 @@ class MQStatusInfo: NSObject {
     var text: String?
     /// 微博来源
     var source: String?
+    /// 用户模型 － 如果直接使用 KVC，会变成字典
+    var user: MQUserInfo?
     
     // MARK: - 构造函数
     // NSArray & NSDictionary 在 swfit 中极少用，contentOfFile  加载 plist 才会用
@@ -27,10 +29,22 @@ class MQStatusInfo: NSObject {
         setValuesForKeysWithDictionary(dict)
     }
     
+    override func setValue(value: AnyObject?, forKey key: String) {
+        // 判断 key 是否是 "User"
+        if key == "user" {
+            // 如果 key 是 user, value 是字典
+            // 调用 User 的构造函数创建 user 对象属性
+            user = MQUserInfo(dict: value as! [String: AnyObject])
+            // 如果不return，user 属性又会被默认的 KVC 方法，设置成字典
+            return
+        }
+        super.setValue(value, forKey: key)
+    }
+    
     override func setValue(value: AnyObject?, forUndefinedKey key: String) {}
     
     override var description: String{
-        let keys = ["created_at", "id", "text", "source"]
+        let keys = ["created_at", "id", "text", "source","user"]
         return dictionaryWithValuesForKeys(keys).description
     }
 }
---
+++ MQSwiftSinaManager/MQSwiftSinaManager/Classes/Model/MQUserInfo.swift
@@ -0,0 +1,38 @@
+//
+//  MQUserInfo.swift
+//  MQSwiftSinaManager
+//
+//  Created by mengmeng on 16/5/30.
+//  Copyright © 2016年 mengQuietly. All rights reserved.
+//
+
+import Foundation
+
+/// 用户模型
+class MQUserInfo: NSObject {
+
+    /// 用户UID
+    var id: Int = 0
+    /// 友好显示名称
+    var name: String?
+    /// 用户头像地址（中图），50×50像素
+    var profile_image_url: String?
+    /// 认证类型 -1：没有认证，0，认证用户，2,3,5: 企业认证，220: 达人
+    var verified: Int = 0
+    /// 会员等级 1-6
+    var mbrank: Int = 0
+
+    // MARK: - 构造函数
+    init(dict:[String: AnyObject]) {
+        super.init()
+        
+        setValuesForKeysWithDictionary(dict)
+    }
+    
+    override func setValue(value: AnyObject?, forUndefinedKey key: String) {}
+    
+    override var description: String{
+        let keys = ["id", "name", "profile_image_url", "verified", "mbrank"]
+        return dictionaryWithValuesForKeys(keys).description
+    }
+}
