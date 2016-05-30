399b66194a77adbd5ecd32a7e3705a0601ad69c8
文静 <wenjing@txdev.org>
2016-05-30 10:01:41 +0800

    首页：绑定表格数据注释添加

--- MQSwiftSinaManager/MQSwiftSinaManager/Classes/Module/Home/MQHomeController.swift
+++ MQSwiftSinaManager/MQSwiftSinaManager/Classes/Module/Home/MQHomeController.swift
@@ -34,41 +34,48 @@ class MQHomeController: MQBaseTableViewController {
     /// 加载微博数据
     func loadWeiboList() {
         
-        statusesListModel.loadStatuses().subscribeNext({ (error) -> Void in
-            printLog(error)
-            SVProgressHUD.showInfoWithStatus("您的网络不给力")
-            
-        }) { () -> Void in
-            // 刷新表格
-            self.tableView.reloadData()
-        }
-//        statusesListModel.loadStatuses().subscribeNext({ (error) in
-//            printLog("首页加载微博数据失败", logError: true)
-//            SVProgressHUD.showInfoWithStatus("您的网络不给力！")
-//            }) { 
-//                // 刷新表格
-//                self.tableView.reloadData()
+//        statusesListModel.loadStatuses().subscribeNext({ (error) -> Void in
+//            printLog(error)
+//            SVProgressHUD.showInfoWithStatus("您的网络不给力")
+//            
+//        }) { () -> Void in
+//            // 刷新表格
+//            self.tableView.reloadData()
 //        }
+        statusesListModel.loadStatuses().subscribeNext({ (error) in
+            printLog("首页加载微博数据失败", logError: true)
+            SVProgressHUD.showInfoWithStatus("您的网络不给力！")
+            }) { 
+                // 刷新表格
+                self.tableView.reloadData()
+        }
     }
     
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
+// 类似于 OC 的分类，同时可以将遵守的协议方法，分离出来
+extension MQHomeController{
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return statusesListModel.statusList?.count ?? 0
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
+        /// 1. dequeueReusableCellWithIdentifier:indexPath 一定会返回一个cell，必须注册可重用cell
+        /// 注册：registerClass/registerNib(XIB)/在 SB 中指定
+        /// 如果缓冲区 cell 不存在，会使用原型 cell 实例化一个新的 cell
+        /// 2. dequeueReusableCellWithIdentifier，会查询可重用cell，如果注册原型cell，能够查询到，否则，返回 nil
+        /// 需要后续判断 if (cell == nil) ，是在 iOS 5.0 开发使用的
         let cell = tableView.dequeueReusableCellWithIdentifier(MQHomeCellID, forIndexPath: indexPath)
         
+        // 1. 获取微博数据
         let statusInfos = statusesListModel.statusList![indexPath.item] as! MQStatusInfo
-        printLog("cell 中模型类容：\(statusInfos.text)")
-        
+        // 2. 设置数据
         cell.textLabel?.text = statusInfos.text
-        
+        // 3. 返回 cell
         return cell
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-    
 }
